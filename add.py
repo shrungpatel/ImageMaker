1aaaea63ff178a555a8dd11c0a07d1c267b44ed4
shrungpatel <shrungpatel@yahoo.in>
2025-12-27 11:08:18 -0500

    added option for text shadow

--- add.py
+++ add.py
@@ -56,11 +56,16 @@ def add_quote_to_images(quote: str, photos_folder, output_folder, photo, font_ch
                     first_line = False
                 x = (img.width - text_width) / 2 
 
+                if random.choice([True, False]): #shadow
+                    offset = 5
+                else:
+                    offset = 1
+
                 # Add text with black outline for visibility
                 draw.text((x-1, y-1), line, font=font, fill="black")
                 draw.text((x+1, y-1), line, font=font, fill="black")
                 draw.text((x-1, y+1), line, font=font, fill="black")
-                draw.text((x+1, y+1), line, font=font, fill="black")
+                draw.text((x+offset, y+offset), line, font=font, fill="black")
                 draw.text((x, y), line, font=font, fill="white")
                 y += text_height + 20 # the 20 adds a little more spacing
             
