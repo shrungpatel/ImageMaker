b61087116f6a48058416b41691d32cd1138c6b68
shrungpatel <shrungpatel@yahoo.in>
2026-06-24 19:04:08 -0500

    added author name to the quote

--- quotes_files.py
+++ quotes_files.py
@@ -7,8 +7,9 @@ def get_quotes_from_files(quotes_source):
             if filename.lower().endswith('.txt'):
                 with open(os.path.join(quotes_source, filename), 'r', encoding='utf-8') as f:
                     lines = f.readlines()
-                    for line in lines:
-                        quotes.append(line.strip())
+                    author = lines[0].strip()
+                    for line in lines[1:]:
+                        quotes.append(line.strip() + ' - ' + author)
         except Exception as e:
             print("Error reading file: " + filename + " - " + str(e))
     return quotes
